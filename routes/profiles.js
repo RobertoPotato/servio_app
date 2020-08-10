43ad3004f6ed60a4929abe748f1175c072fdaefc
RobertoPotato <robertwabuti@gmail.com>
2020-08-10 20:11:50 +0300

    changes to the profiles.js and other routes

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { User, Profile } = require("../models/index");
+const { User, Profile, Tier, Role } = require("../models/index");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -16,18 +16,34 @@ router.post("/", async (req, res) => {
   res.send(profile);
 });
 
+//TODO find the profile of a given user
+router.get("/:userid", async (req, res) => {
+  const profiles = await Profile.findOne({
+    where: { userId: req.params.userid },
+    attributes: {
+      exclude: ['id', 'userId', 'tierId', 'roleId', 'createdAt']
+    },
+    include: [
+      { model: Tier, attributes: ["title", "description", "badgeUrl"] },
+      { model: Role, attributes: ["title", "description"] },
+    ],
+  });
+
+  res.send(profiles);
+});
+
 //Update a database entry
 router.put("/:id", async (req, res) => {
-    //use the logged n user's id to find a profile whose
-    //userId column value matches this
-    //change the data in that profile as instructed
-    //return the new profile data
+  //use the logged n user's id to find a profile whose
+  //userId column value matches this
+  //change the data in that profile as instructed
+  //return the new profile data
   const user = await User.update(
     {
-        bio: req.body.bio,
-        picture: req.body.picture,
-        avatar: req.body.avatar,
-        phoneNumber: req.body.phoneNumber,
+      bio: req.body.bio,
+      picture: req.body.picture,
+      avatar: req.body.avatar,
+      phoneNumber: req.body.phoneNumber,
     },
     {
       where: {
@@ -36,7 +52,7 @@ router.put("/:id", async (req, res) => {
     }
   );
 
-  res.send('okay');
+  res.send("okay");
 });
 
 module.exports = router;
